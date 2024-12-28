39c4281b0f379624aced9bb4a5c05864f264754a
Sarthak Garg <sarthak@Sarthaks-MacBook-Air.local>
2024-12-28 21:52:20 +0530

    first commit

---
+++ app/api/user/route.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+
+import client from "@/db"
+
+export async function POST(req : NextRequest){
+    const body = await req.json();
+
+    try{
+        await client.positions.create({
+            data : {
+                userId : "test",
+                side : "BUY",
+                price : 1,
+                avgPrice : 1,
+                quantity : 1,
+                symbol : "BTC",
+            }
+        })
+        return NextResponse.json({
+            body
+        })
+    }catch(e){
+        return NextResponse.json({
+            error : e
+        })
+    }
+
+
+}
